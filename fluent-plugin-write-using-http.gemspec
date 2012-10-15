0cc5c75a0bd1cb0b969883c02cb47219d55249c7
Jongtae Lee <jt.jongtae@gmail.com>
2012-10-15 11:40:06 +0900

    added code

--- fluent-plugin-write-using-http.gemspec
+++ fluent-plugin-write-using-http.gemspec
@@ -2,12 +2,12 @@
 $:.push File.expand_path("../lib", __FILE__)
 
 Gem::Specification.new do |s|
-  s.name        = "fluent-plugin-daum-QOS"
+  s.name        = "fluent-plugin-write-using-http"
   s.version     = "1.0.0"
   s.authors     = ["Jongtae Lee"]
   s.email       = ["jp-jongtae@daumcorp.com"]
-  s.homepage    = "https://github.com/y-ken/fluent-plugin-rewrite-tag-filter"
-  s.summary     = %q{Fluentd Output filter plugin. It has designed to rewrite tag like mod_rewrite. Also you can change a tag from apache log by domain, status-code(ex. 500 error), user-agent, request-uri, regex-backreference and so on with regular expression.}
+  s.homepage    = "http://www.daum.net"
+  s.summary     = %q{Fluentd Output write plugin. It has designed to write data using http url.}
   # s.description = %q{Fluentd Output filter plugin. It has designed to rewrite tag like mod_rewrite. Also you can change a tag from apache log by domain, status-code(ex. 500 error), user-agent, and request-uri and so on with regular expression.}
 
   s.files         = `git ls-files`.split("\n")
--- lib/fluent/plugin/out_write_using_http.rb
+++ lib/fluent/plugin/out_write_using_http.rb
@@ -1,37 +1,37 @@
-module Fluent
-
-
+require 'httpclient'
+require "socket"
 class HttpRequestOutput < Fluent::BufferedOutput
-  Fluent::Plugin.register_output('httpRequest', self)
+  Fluent::Plugin.register_output('write_using_http', self)
 
   def initialize
     super
     require 'rubygems'
+    
   end
 
   def configure(conf)
     super
 
-    if host = conf['host']
-      @host = host
-    unless @host
-      raise ConfigError, "'host' parameter is required on file output"
-    end 
+#    if host = conf['host']
+#      @host = host
+#    end
+#    unless @host
+#      raise ConfigError, "'host' parameter is required on file output"
+#    end 
 
-
-    if table  = conf['table']
-      @table = table
-    end 
-    unless @table
-      raise ConfigError, "'hbase_table' parameter is required on file output"
-    end 
+#    if table  = conf['table']
+#      @table = table
+#    end 
+#    unless @table
+#      raise ConfigError, "'hbase_table' parameter is required on file output"
+#    end 
 
   end 
 
   def start
     super
     # FIXME : authentication may be required.
-    @client = HTTPClient.new
+    print "start write_using_http\n"
   end 
 
   def shutdown
@@ -41,13 +41,14 @@ class HttpRequestOutput < Fluent::BufferedOutput
     [time, record].to_msgpack
   end
 
-  def write(chunk)
-    
+  def write(chunk)   
+        
+    data = chunk.read
+#    print data
+    http = HTTPClient.new
+    hostname = Socket.gethostname
     chunk.msgpack_each { |time, record|
-      print record
-      @client.get "http://group1.magpie.daum.net/magpie/put/clix" , "code" => "500"
+      http.get("http://group1.magpie.daum.net/magpie/put/clix",{"hostname" => hostname, "message" => record["message"],"location" => record["location"]}) 
     }
-  
-   end 
-
+  end 
 end
